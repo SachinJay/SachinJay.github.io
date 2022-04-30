c619e2b84796086b7b0fa62ba6f96187a5500da5
SachinJay <sjd232@cornell.edu>
2022-04-30 16:56:10 -0400

    Refactored the code a bit

--- src/app.js
+++ src/app.js
@@ -1,25 +1,7 @@
-import { loadTheModel,display_prediction,read_in_image} from './util';
+import {read_in_image} from './util';
 read_in_image();
-
 console.log('Hello, world!');
 
-
-
-
-
-// EVERYTHING YOU WANNA DO WITH THE MODEL NEEDS TO BE AFTER PROMISE IS RESOLVED
-// I.E. You must run prediction in here
-// OR maybe not, maybe it's best to run prediction in the app.js file and have it
-// be contingent on the event listener
-loadTheModel().then(x => {
-    var model = x;
-    console.log("Logging the model from the app.js file:");
-    console.log(model);
-});
-
-var prediction = "Positive example";
-
-display_prediction(prediction);
 
 
 
--- src/util.js
+++ src/util.js
@@ -15,9 +15,6 @@ const cifar10_labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 
 
 /**
  * Function to load the model
- * TODO should this be based on a hosted URL or a locally served file? Have had
- * a lot of trouble trying to get both to work
- * 
  * @returns the pre-trained model
  */
 export async function loadTheModel() {
@@ -25,19 +22,6 @@ export async function loadTheModel() {
     var model = await tf.loadLayersModel(HOSTED_MODEL_URL);
     console.log("Logging the model from the utils file");
     console.log(model);
-
-    /**test model on some nonsense tensors */ 
-    const a = tf.zeros([2,32,32,3]);
-    const p = model.predict(a);
-    const p_argmax = p.argMax(1);
-    const p_array = p_argmax.array();
-
-    p_array.then(preds=> {
-        console.log('Promise is resolved:');
-        console.log(`The prediction is ${cifar10_labels[preds[0]]}`);
-    });
-    /** End the test on non-sense tensors */
-
     return await model;
 }
 
@@ -62,35 +46,12 @@ export function read_in_image(){
                 // Display image in the #img img tag
                 document.querySelector('#img').src = uploaded_image;
                 
-                
+                // HTMLImageElement to predict on
                 var myImage = new Image();
                 myImage.src = uploaded_image;
 
                 myImage.onload = function() {
-
-                    var t = get_img_tensor(myImage);
-                    // console.log('The tensor is: ');
-                    // t.print(true);
-
-                    t.array().then(arr =>{
-                        const a = preprocess_img(arr)
-                        console.log('The wrapped tensor is: ');
-                        a.print(true);
-
-                        loadTheModel().then(model=>{
-                            const p = model.predict(a);
-                            const p_argmax = p.argMax(1);
-                            const p_array = p_argmax.array();
-
-                            p_array.then(preds=> {
-                                display_prediction(cifar10_labels[preds[0]])
-                            });
-                        });
-                    });
-
-                    // TODO: add inference/preprocessing logic here
-
-
+                    predict_image(myImage);
                 }
             });
             reader.readAsDataURL(this.files[0]);
@@ -109,7 +70,12 @@ function preprocess_img(img_arr){
 
     // wrap the array and convert to tensor
     // Do this because the predict function expects array of many exmaples
-    const a = tf.tensor([img_arr]);
+    var a = tf.tensor([img_arr]);
+
+    // Resize to 32x32, not necessary if you're uploading an image from
+    //The cifar data set
+    //Also, normalize by dividing by 255
+    a = tf.image.resizeBilinear(a,[32,32]).div(tf.scalar(255));
 
     return a;
 }
@@ -130,6 +96,39 @@ function get_img_tensor(myImage){
     var t = tf.browser.fromPixels(myImage,3);
 
     return t;
+}
+
+/**
+ * Runs the model on the input image and displays the prediction on the site
+ * 
+ * @param {HTMLImageElement} image 
+ */
+function predict_image(image){
+
+    // Convert the image to a tensor   
+    var t = get_img_tensor(image);
+
+    // Convert the tensor to an array
+    t.array().then(arr =>{
+
+        // Pre-process the array
+        const a = preprocess_img(arr);
+
+        //Load the model
+        loadTheModel().then(model=>{
+
+            // Run the prediction
+            const p = model.predict(a);  //Soft max probability vector
+            const p_argmax = p.argMax(1);//Tensor of indices of highest probability labels
+            const p_array = p_argmax.array();//Convert tensor to array for indexing
+
+            p_array.then(preds=> {
+                // Once tensor is converted to array, use labels array
+                // To properly name the prediction 
+                display_prediction(cifar10_labels[preds[0]])
+            });
+        });
+    });
 }
 
 
